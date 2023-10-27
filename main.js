a7c3b174cfe1efb93aa10fc64e6e2cd3da74c716
intangibleSquid <ckcoy@iu.edu>
2023-10-27 09:46:46 -0400

    first homepage commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// toggle "on page" links (on click)
+function toggle_on_page_links() {
+    document.getElementById("onPageLinks").classList.toggle("show");
+    document.getElementById("expandNavlinks").classList.toggle("hide");
+}
