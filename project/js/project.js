920e7b19ca63bf480e2542111c9c80aa5b06f258
intangibleSquid <ckcoy@iu.edu>
2023-11-12 03:05:43 -0500

    fixed navbar, css, responsive media queries, cleaned up comments

--- project/js/project.js
+++ project/js/project.js
@@ -1,7 +1,7 @@
 // toggle "on page" links (on click)
 function toggle_on_page_links() {
-    document.getElementById("onPageLinks").classList.toggle("show");
-    document.getElementById("expandNavlinks").classList.toggle("hide");
+    document.getElementById("onPageLinks").classList.toggle("show");  // toggle the 'show' css rule (on 'onPageLinks')
+    document.getElementById("expandNavlinks").classList.toggle("hide");  // toggle the 'hide' css rule (on 'expandNavlinks')
 }
 
 // open trivia app window (on clicking "popup" link / anchor tag)
@@ -14,5 +14,5 @@ function trivia_app_popup() {
     var windowFeatures = 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top;  // set the window features (size and placement)
     window.open(url, 'popup', windowFeatures);  // call window.open() to create a popup window
     // window.open('app/TriviaApp.html','popup','width=500,height=875');  // original code, prior to centering window
-    return false;
+    return false;  // stop the function (return false)
 }
